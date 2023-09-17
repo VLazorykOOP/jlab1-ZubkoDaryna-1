7faab7b48ffa945e1271e4e707334c095f6676bc
ZubkoDaryna <124194246+ZubkoDaryna@users.noreply.github.com>
2023-09-17 23:00:05 +0300

    secondTask

---
+++ src/secondTask.java
@@ -0,0 +1,73 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class secondTask {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+        System.out.println("Second Task");
+
+        System.out.println("A array(<500):");
+         int[] A= Input(in,500);
+        System.out.println("B array(<200):");
+
+         int[] B= Input(in,200);
+
+        int[] unionArray = Union(A, B);
+        System.out.println("A Y B:");
+        Print(unionArray);
+        int sum = sumOfUnion(unionArray);
+        System.out.println("Sum of elements: " + sum);
+                in.close();
+    }
+
+
+    static int[] Input(Scanner in, int maxSize) {
+        System.out.println("Size of array=");
+        int n = in.nextInt();
+      
+        if (n >= maxSize) {
+            System.out.println("Error: Array size should be less than maxSize");
+            System.exit(1);
+        }    
+
+             int[] a = new int[n];
+        for (int i = 0; i < n; ++i) {
+            System.out.print("a[" + i + "] = ");
+            a[i] = in.nextInt();
+        }
+        return a;
+    }
+    public static void Print(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a["+i+"]="+a[i] +" " );
+        }
+        System.out.println();
+    }
+    public  static int[] Union(int[] a, int[] b) {
+        HashSet<Integer> union = new HashSet<>();
+        for (int num : a) {
+            union.add(num);
+        }
+        for (int num : b) {
+            union.add(num);
+        }
+    
+        int[] unionArray = new int[union.size()];
+        int index = 0;
+    
+        for (int num : union) {
+            unionArray[index] = num;
+            index++;
+        }
+    
+        return unionArray;
+    }
+    public static int sumOfUnion(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return sum;
+    }
+}
