c60bfb0c9fca605cd7c0b9325604cf79abef7c7e
ZubkoDaryna <124194246+ZubkoDaryna@users.noreply.github.com>
2023-09-21 22:28:31 +0300

    task

--- src/Fourth.java
+++ src/Fourth.java
@@ -4,25 +4,6 @@ import java.util.Set;
 
 public class Fourth {
     public static void main(String[] args) {
-        String inputString = "rfvbr tghht"; // Замініть це на ваш рядок
-        
-        // Створюємо множину для відстеження вже зустрічених букв
-        Set<Character> seenLetters = new HashSet<>();
-        
-        // Будемо зберігати результат у цій змінній
-        StringBuilder result = new StringBuilder();
-        
-        // Проходимося по кожному символу у рядку
-        for (int i = 0; i < inputString.length(); i++) {
-            char letter = inputString.charAt(i);
-            
-            // Якщо буква ще не була зустрінута раніше, додаємо її до результату
-            if (!seenLetters.contains(letter)) {
-                result.append(letter);
-                seenLetters.add(letter);
-            }
-        }
-        
-        System.out.println("Результат: " + result.toString());
+
     }
 }
