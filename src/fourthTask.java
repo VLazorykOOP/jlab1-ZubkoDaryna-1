a098246a1b2b28f09b361260008d54e89b563efc
ZubkoDaryna <124194246+ZubkoDaryna@users.noreply.github.com>
2023-09-21 22:23:50 +0300

    FourthTask

--- src/fourthTask.java
+++ src/fourthTask.java
@@ -1,26 +1,33 @@
 import java.util.Scanner;
 
 public class fourthTask {
-    
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-       /* */ System.out.println("Input text:");
+        System.out.println("Input text:");
         String text = in.nextLine();
         String[] words = text.split("[ ,.;:\\-+&!?]");
-        char firstLetter = words[0].charAt(0);
-
+        
         for (int i = 0; i < words.length; i++) {
             String word = words[i];
-            
-            if (i > 0) {
-                word = word.replaceAll(String.valueOf(firstLetter), "");
-                                System.out.print(" " + word);
-            }
-            if (i == 0) {
-                System.out.print(word);
-            } 
+            char firstLetter = word.charAt(0); // 
 
+            for (int j = 1; j < word.length(); j++) {
+                char currentChar = word.charAt(j);
+
+                if (currentChar == firstLetter) {
+                    word = word.substring(0, j) + word.substring(j + 1); 
+                }
+            }
+                if(i>0){
+                System.out.print(" " + word);}
+                else{
+            System.out.print(word);
+                }
+           
         }
         in.close();
     }
 }
+
+
